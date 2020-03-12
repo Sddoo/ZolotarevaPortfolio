33381188175f5aa11a6645749200fded40f9ba5d
Vlados <dkflbr-09@mail.ru>
2020-03-12 11:33:19 +0300

    toggle

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 document.addEventListener('mouseover', function(e) {
     if (e.target.className === 'grid_item') {
-		console.log("hello");
+		// console.log("hello");
     }
+});
+
+document.querySelector(".open_info").addEventListener('click', function() {
+	if ($(".info").css("left") !== '0px') {
+		$(".info").animate({left: "0%",}, 500);
+		$(".grid_container").animate({left: "30%",}, 500);
+	} else {
+		$(".info").animate({left: "-30%",}, 500);
+		$(".grid_container").animate({left: "0%",}, 500);
+	}
 });
